09a5f6a8329f2faf4e2fd649ae7ae388f875af99
saiteja7171 <saiteja7171@gmail.com>
2024-02-06 10:59:05 -0600

    Initial Commit

---
+++ DessertApp MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DessertApp MVVM
+//
+//  Created by Sai Teja Atluri on 2/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
